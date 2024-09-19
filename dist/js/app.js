75e3ffc49712b1dbcfb8bb8705b9fe45e0c545b5
Tweber663 <tweber.1995@hotmail.com>
2024-09-19 15:59:57 +0200

    second hamburger added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,12 @@
 "use strict";
 const toggle = document.getElementById('toggle');
-const nav = document.getElementById('nav');
+const toggle2 = document.getElementById('toggle2');
+const nav = document.getElementById('nav1');
+const nav2 = document.getElementById('nav2');
 toggle === null || toggle === void 0 ? void 0 : toggle.addEventListener('click', () => {
     nav === null || nav === void 0 ? void 0 : nav.classList.toggle('active');
+});
+toggle2 === null || toggle2 === void 0 ? void 0 : toggle2.addEventListener('click', () => {
+    nav2 === null || nav2 === void 0 ? void 0 : nav2.classList.toggle('active');
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 const toggle = document.getElementById('toggle'); 
-const nav = document.getElementById('nav');
+const toggle2 = document.getElementById('toggle2'); 
+const nav = document.getElementById('nav1');
+const nav2 = document.getElementById('nav2');
 
 
 toggle?.addEventListener('click', () => {
     nav?.classList.toggle('active');
+})
+
+toggle2?.addEventListener('click', () => {
+    nav2?.classList.toggle('active');
 })
