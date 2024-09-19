ff90eb01de371979dae4773fae20fd81826ac141
Tweber663 <tweber.1995@hotmail.com>
2024-09-19 13:59:22 +0200

    js added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,2 +1,7 @@
 "use strict";
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+toggle === null || toggle === void 0 ? void 0 : toggle.addEventListener('click', () => {
+    nav === null || nav === void 0 ? void 0 : nav.classList.toggle('active');
+});
 //# sourceMappingURL=app.js.map
